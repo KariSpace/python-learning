ea91a5befb6c8b35927781f1f5f27cd210296d0b
KariSpace <kpivtorak55@gmail.com>
2022-08-27 03:12:10 +0300

    Level order tree traversal

--- binary-tree/binary-tree.py
+++ binary-tree/binary-tree.py
@@ -1,3 +1,27 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
 class Node(object):
 	def __init__(self, value): 
 		self.value = value
@@ -15,7 +39,6 @@ class BinaryTree(object):
 			return self.inorder_print(tree.root, '')
 		if traversal_type == 'postorder':
 			return self.postorder_print(tree.root, '')
-
 
 	# pre-order
 	def preorder_print(self, start, traversal):
@@ -40,6 +63,39 @@ class BinaryTree(object):
 			traversal += (str(start.value) + ' ')
 		return traversal
 
+	# level-order
+	def levelorder_wrong_print(self, start):
+
+		if start:
+			# print(start.value)
+			if(start.left):
+				print(start.left.value)
+			if(start.left):
+				print(start.right.value)
+			traversal = self.levelorder_print(start.left)
+			traversal = self.levelorder_print(start.right)
+
+		return traversal
+	# level-order
+	def levelorder_print(self, start):  
+
+		if start is None:
+			return 
+		queue = Queue()
+		queue.enqueue(start)
+
+		traversal = ""
+		while len(queue) > 0:
+			traversal += str(queue.peek()) + " "
+			node = queue.dequeue()
+			
+			if node.left:
+				queue.enqueue(node.left)
+			if node.right:
+				queue.enqueue(node.right)
+
+		return traversal
+
 # Full binary tree
 tree = BinaryTree(1)
 tree.root.left = Node(2)
@@ -49,7 +105,23 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
-print(tree.print_tree('preorder'))
-print(tree.print_tree('inorder'))
-print(tree.print_tree('postorder'))
+tree.root.left.left.left = Node(8)
+tree.root.left.left.right = Node(9)
+tree.root.left.right.left = Node(10)
+tree.root.left.right.right = Node(11)
+
+tree.root.right.left.left = Node(12)
+tree.root.right.left.right = Node(13)
+tree.root.right.right.left = Node(14)
+tree.root.right.right.right = Node(15)
+
+
+# print(tree.preorder_print(tree.root, ''))
+
+# print(tree.inorder_print(tree.root, ''))
+
+# print(tree.postorder_print(tree.root, ''))
+
+print(tree.levelorder_print(tree.root))
+
 
